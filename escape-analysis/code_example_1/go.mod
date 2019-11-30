943a318faa5309a194c01525ca91bf687bcfda13
PumpkinSeed <qwer.kocka@gmail.com>
2019-11-30 13:22:50 +0100

    Add escape analysis

---
+++ escape-analysis/code_example_1/go.mod
@@ -0,0 +1,3 @@
+module github.com/PumpkinSeed/adventure-of-golang/escape-analysis/code_example_1
+
+go 1.13
---
+++ escape-analysis/code_example_1/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	testdata := "test"
+	println("main:5\tvalue:", testdata, "\taddress:", &testdata)
+
+	f(testdata)
+	println("main:5\tvalue:", testdata, "\taddress:", &testdata)
+}
+
+//go:noinline
+func f(str string) {
+	str += "_f"
+	println("main:5\tvalue:", str, "\taddress:", &str)
+}
